597b9148e9a613fb679aadc3153bbb052feef18e
Jonathan Sawali <dsawali@sfu.ca>
2016-03-15 18:31:11 -0700

    remove long ass comment

--- tester.cpp
+++ tester.cpp
@@ -254,24 +254,7 @@ SUITE(GET) {
     CHECK_EQUAL(status_codes::OK, delete_entity (GetFixture::addr, GetFixture::table, partition, row));
   }
 
-  // // A test of GET all table entries with specific properties
-  // TEST_FIXTURE(GetFixture, GetAllProp){
-  //   string partition {"Sheen,Charlie"};
-  //   string row {"USA"};
-  //   string property {"problem"};
-  //   string prop_val {"drugs"};
-  //   int put_result {put_entity(GetFixture::addr, GetFixture::table, partition, row, property,prop_val)};
-  //   cerr << "put result " << put_result << endl;
-  //   assert (put_result ++ status_codes::OK);
-
-  //   pair<status_code,value> result {
-  //     do_request (methods::GET,
-  //       string(GetFixture::addr)
-  //       +_string(GetFixture::table)+)
-  //   }
-  // }
-
-TEST_FIXTURE(GetFixture, GetProperty) {
+  TEST_FIXTURE(GetFixture, GetProperty) {
     string partition {"Edmund"};
     string row {"Ottawa"};
     string property {"Born"};
