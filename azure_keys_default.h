dd8719491e2a0bc9aad27dd5903486ce15bd15a1
qinxin xu <qinxinx@sfu.ca>
2016-03-25 17:15:33 -0700

    Updated azure_keys_default.h (I don't know why.)

--- azure_keys_default.h
+++ azure_keys_default.h
@@ -4,7 +4,7 @@
 #include <string>
 
 const std::string storage_connection_string {"DefaultEndpointsProtocol=https;"
-	"AccountName=;"
-	"AccountKey="};
+	"AccountName=qinxinxstorage;"
+	"AccountKey=hhMRhqfoW5QzESkFanU53ymO8LUI6QvLXf/+7F2y7dzIz+JC3iCcV45QQYJClXC7IwrTTvyQORqktfW5857fhw=="};
 
 #endif
